5a912afbaa7bb869fe26f411a4cb0bfebe13cc9d
Emily Park <eppark@caltech.edu>
2020-08-03 14:45:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
+
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -17,16 +18,37 @@ if (process.env.FCM_API_KEY) {
     pushConfig['android'] = { apiKey: process.env.FCM_API_KEY || ''};
 }
 
+if (process.env.APNS_ENABLE) {
+    pushConfig['ios'] = [
+        {
+            pfx: 'ParsePushDevelopmentCertificate.p12', // P12 file only
+            bundleId: 'beta.codepath.parsetesting',  // change to match bundleId
+            production: false // dev certificate
+        }
+    ]
+}
+
+var filesAdapter = null;  // enable Gridstore to be the default
+if (process.env.S3_ENABLE) {
+    var S3Adapter = require('parse-server').S3Adapter;
+
+    filesAdapter = new S3Adapter(
+        process.env.AWS_ACCESS_KEY,
+        process.env.AWS_SECRET_ACCESS_KEY,
+        {bucket: process.env.AWS_BUCKET_NAME, bucketPrefix: "", directAccess: true}
+    );
+}
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   push: pushConfig,
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  filesAdapter: filesAdapter,
+  liveQuery: { classNames: ["Message"]},
+  publicServerURL: process.env.SERVER_URL || 'http://localhost/parse',
+  serverURL: process.env.SERVER_URL || 'http://localhost/parse'  // needed for Parse Cloud and push notifications
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -44,12 +66,6 @@ app.use(mountPath, api);
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
