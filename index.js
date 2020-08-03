869f72faeddcc8d14157dc363cf10c286f4eba21
Emily Park <eppark@caltech.edu>
2020-08-03 10:47:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.DB_URI
+
+if (process.env.FCM_API_KEY) {
+   pushConfig['android'] = { 
+   apiKey: process.env.FCM_API_KEY || ''};
+}
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -16,6 +21,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  push: pushConfig,
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
