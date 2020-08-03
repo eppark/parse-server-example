c2b26eb545f46efb9183ce0714a9722a2cfbdd20
Emily Park <eppark@caltech.edu>
2020-08-03 11:37:14 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -567,30 +567,16 @@ Parse.Cloud.define('pushChannelTest', function (request, response) {
 
     // request has 2 parameters: params passed by the client and the authorized user
     var params = request.params;
-    var user = request.user;
 
     var customData = params.customData;
-    var launch = params.launch;
-    var broadcast = params.broadcast;
-
-    // use to custom tweak whatever payload you wish to send
-    var pushQuery = new Parse.Query(Parse.Installation);
-    pushQuery.equalTo("deviceType", "android");
-
-    var payload = {};
-
     if (customData) {
-        payload.customdata = customData;
-    } else if (launch) {
-        payload.launch = launch;
-    } else if (broadcast) {
-        payload.broadcast = broadcast;
+        payload.alert = customData;
     }
 
     // Note that useMasterKey is necessary for Push notifications to succeed.
 
     Parse.Push.send({
-        where: pushQuery, // for sending to a specific channel
+        channels: ["default"],
         data: payload,
     }, {
         success: function () {
